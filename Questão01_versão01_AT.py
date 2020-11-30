035dbae5a6edfe738711f5b1c77f221e138f7017
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-11-30 17:59:17 -0300

    Create Questão01_versão01_AT.py

---
+++ Questão01_versão01_AT.py
@@ -0,0 +1,12 @@
+#1) Usando o Thonny, escreva um programa em Python que leia uma tupla contendo 3 números inteiros, (n1, n2, n3) e os imprima em ordem crescente.
+# Versão 01
+
+def order(t):
+    return sorted(t)    
+
+  
+t = (10, 15, 1)
+
+print(f"Essa é a sua tupla: {t}")
+
+print(f"Essa é a sua tupla ordenada: {tuple(order(t))}")
