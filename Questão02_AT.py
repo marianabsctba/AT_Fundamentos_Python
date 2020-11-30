c223ab69b31f53cdb8276c8e97eac6c669504cd4
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-11-30 18:03:12 -0300

    Create Questão02_AT.py

---
+++ Questão02_AT.py
@@ -0,0 +1,2 @@
+#Usando o Thonny, escreva um programa em Python que some todos os números pares de 1 até um dado n, inclusive.
+#O dado n deve ser obtido do usuário. No final, escreva o valor do resultado desta soma.
