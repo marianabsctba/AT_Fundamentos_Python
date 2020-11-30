8799a78afcf169c41ab238a55708f6e8c4ab87d9
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-11-30 19:00:32 -0300

    Create Questão06_AT.py

---
+++ Questão06_AT.py
@@ -0,0 +1,4 @@
+#6) Escreva uma função em Python que:
+#leia uma tupla contendo números inteiros,
+#retorne uma lista contendo somente os números ímpares
+#e uma nova tupla contendo somente os elementos nas posições pares.
