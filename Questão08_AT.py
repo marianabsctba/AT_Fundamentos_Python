90be63f7ac8f26e945f190a36f6411be5165582c
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-12-02 01:28:49 -0300

    Update Questão08_AT.py

--- Questão08_AT.py
+++ Questão08_AT.py
@@ -1,79 +1,100 @@
-import pygame, random, math
+import pygame, random, math, sys
+from pygame.locals import *
 
-w = 800
-h = 600
+W = 800
+H = 600
 
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 HOVER_COLOR = 50, 70, 90
 
-square_list = []
+SQUARE_LIST = []
 
 pygame.init()
-screen = pygame.display.set_mode((w, h))
+screen = pygame.display.set_mode((W, H))
+pygame.display.set_caption("Questão 08 - Fund. Python - Mariana B. Sukevicz")
 pygame.font.init()
 clock = pygame.time.Clock()
 
-
 running = True
 
-CLOCK_TICK = 20
+CLOCK_TICK = 100
 
-rad = 100
-rx = w // 2
-ry = rad
-a = (rx, ry)
+RAD = 100
+RX = W // 2
+RY = RAD
+A = (RX, RY)
 
-class Square():
+class Rectangle():
     def __init__(self):
-        self.w = 60
-        self.h = 30
-        self.x = random.randint(0, w - self.w)
-        self.y = random.randint(20, h - self.h)
-        self.s_a = pygame.Rect(self.x, self.y, self.w, self.h)
+        self.W = 60
+        self.H = 30
+        self.x = random.randint(0, W - self.W)
+        self.y = random.randint(20, H - self.H)
+        self.s_a = pygame.Rect(self.x, self.y, self.W, self.H)
         self.color = (random.randint(20, 255), random.randint(20, 255), random.randint(20, 255))
 
-    def drawSquare(self, screen):
+    def drawRectangle(self, screen):
         pygame.draw.rect(screen, self.color, self.s_a)
         
 
-def drawCircle(rad, rx, ry):
-    pygame.draw.circle(screen, HOVER_COLOR, (rx, ry), rad)
+def drawCircle(RAD, RX, RY):
+    pygame.draw.circle(screen, HOVER_COLOR, (RX, RY), RAD)
     font = pygame.font.SysFont('Times New Roman', 20)
     text = font.render("CLIQUE AQUI!", 1, WHITE)
-    show_text = (rx - (rad // 2), ry - 25)
+    show_text = (RX - (RAD // 2), RY - 25)
     screen.blit(text, show_text)
-    
-    
+
+
+
+def detectCollision(rleft, rtop, width, height, center_x, center_y, RAD):
+    rright, rbottom = rleft + width / 2, rtop + height / 2
+    cleft, ctop = center_x - RAD, center_y - RAD
+    cright, cbottom = center_x + RAD, center_y + RAD
+    if rright < cleft or rleft > cright or rbottom < ctop or rtop > cbottom:
+        return False
+
+    for x in (rleft, rleft + width):
+        for y in (rtop, rtop + height):
+            if math.hypot(x - center_x, y - center_y) <= RAD:
+                return True
+
+    if rleft <= center_x <= rright and rtop <= center_y <= rbottom:
+        return True
+    return False
+
 while running:
     show = True
     screen.fill(WHITE)
     
-    for q in square_list:
-        q.drawSquare(screen)
-    
+    for c in SQUARE_LIST:
+        c.drawRectangle(screen)    
     for event in pygame.event.get():       
         if event.type == pygame.QUIT:
             running = False
         
+       
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
             click = pygame.mouse.get_pos()
-            collision_p = math.hypot(click[0] - rx, click[1] - ry)
-            if collision_p <= rad:
-                s = Square()
-                s.drawSquare(screen)
-                
-                for s_s in square_list:
-                    if s_s.s_a.colliderect(s.s_a):
-                        square_list.remove(s_s)
+            collision_p = math.hypot(click[0] - RX, click[1] - RY)
+            if collision_p <= RAD:
+                r = Rectangle()
+                r.drawRectangle(screen)              
+                for r_r in SQUARE_LIST:
+                    if r_r.s_a.colliderect(r.s_a):
+                        SQUARE_LIST.remove(r_r)
                         show = False
                 if show:
-                    square_list.append(s)
+                    SQUARE_LIST.append(r)    
 
-    circle = drawCircle(rad, rx, ry)
+    circle = drawCircle(RAD, RX, RY)
     pygame.display.update()
 
     clock.tick(CLOCK_TICK)
 
 pygame.display.quit()
 pygame.quit()
+sys.exit()
+
+if __name__ == '__main__':
+    main()  
