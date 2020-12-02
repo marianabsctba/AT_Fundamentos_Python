56ee8f39bcf7d67648719359a995e79623a4dfea
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-12-02 01:39:34 -0300

    Update Questão07_AT.py

--- Questão07_AT.py
+++ Questão07_AT.py
@@ -1,35 +1,35 @@
 #7) Usando a biblioteca ‘pygame’, escreva um programa que desenha na tela em posição aleatória um quadrado amarelo de tamanho 50 (cinquenta), 
 #toda vez que a tecla espaço for pressionada ou o botão direito for clicado.
 
-import pygame, random
+iimport pygame, random
 from pygame.locals import *
 
-square_l = []
+SQUARE_L = []
 
 
-width_s = 800
-weight_s = 600
+WIDTH_S = 800
+HEIGHT_S = 600
 
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+SCREEN = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Questão 07 - AT - Fund. Python - Mariana B. Sukevicz")
 running = True
 
-yellow = 255, 255, 0
-black = 0, 0, 0
+YELLOW = 255, 255, 0
+BLACK = 0, 0, 0
 
 
 class Square:
     def update(self):
         self.height = 50
         self.width = 50
-        self.x = random.randint(0, width_s - self.width)
-        self.y = random.randint(0, weight_s - self.height)
+        self.x = random.randint(0, WIDTH_S - self.width)
+        self.y = random.randint(0, HEIGHT_S - self.height)
         self.square = pygame.Rect(self.x, self.y, self.height, self.width)
 
     def draw(self): 
-        pygame.draw.rect(screen, yellow, self.square)
+        pygame.draw.rect(SCREEN, YELLOW, self.square)
 
 square = Square()
 d_square = False
@@ -42,21 +42,22 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:   
             square = Square()
             square.update()
-            square_l.append(square)
+            SQUARE_L.append(square)
             d_square = True
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 square = Square()
                 square.update()
-                square_l.append(square)
+                SQUARE_L.append(square)
                 d_square = True
        
-    screen.fill((black))
+    SCREEN.fill((BLACK))
     if d_square:           
-        for square in square_l:
+        for square in SQUARE_L:
             square.draw()
 
-    pygame.display.flip()
+    pygame.display.update()
 
 
+pygame.display.quit()
 pygame.quit()
