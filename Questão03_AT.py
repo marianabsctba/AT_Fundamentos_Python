57428a5bc29ad598a7e79a29589eaa1c86767da7
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-12-03 00:39:29 -0300

    Update Questão03_AT.py

--- Questão03_AT.py
+++ Questão03_AT.py
@@ -2,12 +2,18 @@
 #e calcular AB usando multiplicações sucessivas (não use a função de python math.pow) e retornar o resultado da operação.
 #Depois, crie um programa em Python que obtenha dois números inteiros do usuário e indique o resultado de AB usando a função.
 
-def potency(A, B):
-    pot = A ** B
-    return pot
+def mult_a_b(a, b):
+    sums = sum(b for _ in range(abs(a)))
+    return sums 
 
+def pot(a, b):
+    sums = 1
+    for _ in range(b):
+        sums = mult_a_b(sums, a)
+    return sums
 
-A = int(input("Digite um número (base): "))
-B = int(input("Digite um número (potência): "))
+a = int(input("Digite a base: "))
+b = int(input("Digite o expoente: "))
 
-print(f"Base: {A} -- Expoente: {B} -- Resultado: {potency(A, B)}")
+print(f"Base: {a}  - Expoente: {b}")
+print(f"Resultado: {pot(a, b)}")
