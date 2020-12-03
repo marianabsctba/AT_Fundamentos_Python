87f1ee4428444545428fb578b90d95aad612e39f
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-12-03 04:58:41 -0300

    Update Questão06_versão02_AT.py

--- Questão06_versão02_AT.py
+++ Questão06_versão02_AT.py
@@ -2,19 +2,20 @@
 #leia uma tupla contendo números inteiros,
 #retorne uma lista contendo somente os números ímpares
 #e uma nova tupla contendo somente os elementos nas posições pares.
- 
- 
- # alteração do exercício para trabalhar com listas e tuplas e input do usuário
- 
+
+#acrescentei tupla com os números pares também, além do index. Nessa versão, ainda, incluí input do usuário e uso de listas
+
 def read_t(tupl):
     return tuple(tupl)
 
-def show_i(tupl):
+def show_int(tupl):
     return [n for n in tupl if n % 2 != 0]
 
 def show_p(tupl):
     return tuple(n for n in tupl if n % 2 == 0)
 
+def show_index(tupl):
+    return tuple(n for i, n in enumerate(tupl) if i % 2 == 0)
 
 t = []
 n = int(input("Digite o número de elementos na tupla: "))
@@ -22,10 +23,11 @@ for e in range(1, n+1):
     user_t = int(input(f"Digite o {e}° número inteiro: "))
     t.append(user_t)
 
-
-l_i = show_i(t)
+l_i = show_int(t)
 tup_p = show_p(t)
+s_index = show_index(t)
 
-print(f"Tupla digitada: {read_t(t)}")
+print(f"Tupla: {read_t(t)}")
 print(f"Lista de ímpares: {l_i}")
 print(f"Tupla de pares: {tup_p}")
+print(f"Tupla com elementos em posições pares: {s_index}")
