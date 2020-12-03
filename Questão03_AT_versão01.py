e9a29a772e6b23e8dfa18e71285bcee761b962fa
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-12-03 04:16:43 -0300

    Update Questão03_AT_versão01.py

--- Questão03_AT_versão01.py
+++ Questão03_AT_versão01.py
@@ -1,4 +1,4 @@
-#Usando o Thonny, escreva uma função em Python chamada potencia. Esta função deve obter como argumentos dois números inteiros, A e B,
+#3)Usando o Thonny, escreva uma função em Python chamada potencia. Esta função deve obter como argumentos dois números inteiros, A e B,
 #e calcular AB usando multiplicações sucessivas (não use a função de python math.pow) e retornar o resultado da operação.
 #Depois, crie um programa em Python que obtenha dois números inteiros do usuário e indique o resultado de AB usando a função.
 
