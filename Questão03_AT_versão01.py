72bf0fadfac68ecfa28c081a30fd0bc092753fe8
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-12-03 02:30:39 -0300

    Update Questão03_AT_versão01.py

--- Questão03_AT_versão01.py
+++ Questão03_AT_versão01.py
@@ -2,18 +2,14 @@
 #e calcular AB usando multiplicações sucessivas (não use a função de python math.pow) e retornar o resultado da operação.
 #Depois, crie um programa em Python que obtenha dois números inteiros do usuário e indique o resultado de AB usando a função.
 
-def mult_AB(A, B):
-    sums = sum(B for _ in range(abs(A)))
-    return sums 
-
-def exp_AB(A, B):
+def potencia(A, B):
     sums = 1
-    for _ in range(B):
-        sums = mult_AB(sums, A)
+    while B > 0:
+        sums *= A
+        B -= 1
     return sums
-
+   
 A = int(input("Digite a base: "))
 B = int(input("Digite o expoente: "))
-
-print(f"Base: {A}\nExpoente: {B}")
-print(f"Resultado: {exp_AB(A, B)}")
+    
+print(potencia(A, B))
