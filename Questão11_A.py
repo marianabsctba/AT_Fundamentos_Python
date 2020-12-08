9e403199da76e1c870f128abf088b3e31b8d598f
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-12-08 13:54:29 -0300

    Create Questão11_A.py

---
+++ Questão11_A.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+file = "https://fgopassos.github.io/pagina_exemplo/estadosCentroOeste.html"
+html = requests.get(file).text
+soup = BeautifulSoup(html, "lxml")
+
+def printle():   
+    for tabela in soup.find_all('div', class_='tabela'):
+        return tabela.text
+
+
+print(printle())
