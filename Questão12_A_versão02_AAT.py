ca93b502a09c586379207d2c8e0d0c3e8ac71e47
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-12-08 17:30:08 -0300

    Update Questão12_A_versão02_AAT.py

--- Questão12_A_versão02_AAT.py
+++ Questão12_A_versão02_AAT.py
@@ -3,25 +3,27 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-#versão que organiza o contepudo apresentado.
- 
-def data(x):
-    data_lines = x.text.replace('\n','//')[2:-2].split('//')
-    return data_lines
+#versão que organiza o conteúdo apresentado.
 
 file = "https://fgopassos.github.io/pagina_exemplo/estadosCentroOeste.html"
 html = requests.get(file).text
 soup = BeautifulSoup(html, "lxml")
 
-title = []
-lines = []
 
-for table in soup.find_all('div', class_='tabela'):
-    for title_line in table.find_all('div', class_='titulo'):
-        title.append(data(title_line))
-    
-    for line in table.find_all('div', class_='linha'):
-        lines.append(data(line))
+def data(x):
+    data_lines = x.text.replace('\n','//')[2:-2].split('//')
+    return data_lines
 
-df = pd.DataFrame(data=lines, columns=title)
-print(df)
+def tables():
+    title = []
+    lines = []
+    for table in soup.find_all('div', class_='tabela'):
+        for title_line in table.find_all('div', class_='titulo'):
+            title.append(data(title_line))
+        
+        for line in table.find_all('div', class_='linha'):
+            lines.append(data(line))
+
+        return pd.DataFrame(data=lines, columns=title)
+
+print(tables())
